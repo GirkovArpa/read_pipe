ea9f17f883e6945a32c5ff35982e0fd203c18109
GirkovArpa <girkovarpa@protonmail.com>
2021-03-23 17:31:55 -0500

    :bug: handle pipe close, allow custom EOL

--- read_pipe.js
+++ read_pipe.js
@@ -1,11 +1,11 @@
 import { PLATFORM } from '@env';
 import { decode } from '@sciter';
 
-export async function* read_pipe(pipe) {
-  const EOL = PLATFORM === 'Windows' ? '\r\n' : '\n';
+export async function* read_pipe(pipe, EOL = PLATFORM === 'Windows' ? '\r\n' : '\n') {
   const buffer = [];
   while (pipe) {
-    const bytes = await pipe.read();
+    const bytes = await try_read_pipe(pipe);
+    if (bytes === null) return;
     const chars = decode(bytes);
     for (const char of chars) {
       buffer.push(char);
@@ -16,5 +16,13 @@ export async function* read_pipe(pipe) {
         buffer.length = 0;
       }
     }
+  }
+}
+
+function try_read_pipe(pipe) {
+  try {
+    return pipe.read();
+  } catch (e) {
+    return null;
   }
 }
